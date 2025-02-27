8c8c1b497d3072ecb1c4c4590fcbc6d1873f7533
Oluoma <eziolisedivine@gmail.com>
2025-02-27 09:22:15 -0500

    create mobile app

---
+++ Mobile-App/IRIS/IRISApp.swift
@@ -0,0 +1,17 @@
+//
+//  IRISApp.swift
+//  IRIS
+//
+//  Created by Kyle Coon on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct IRISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
